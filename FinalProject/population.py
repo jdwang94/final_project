6ff7d3c72974b6800402c7526b9d6d3aebfb1ac4
jdwang94 <41097683+jdwang94@users.noreply.github.com>
2018-07-14 18:25:42 +0900

    Add files via upload
    add

--- FinalProject/population.py
+++ FinalProject/population.py
@@ -1,22 +1,26 @@
 from read_csv import csv
+import logging
 
 class population(csv):
     def __init__(self):
-        self.columns = []
-        self.data = []
+        csv.__init__(self)
         self.list_of_countries = []
+
 
     def import_csv(self,filename):
         """
         Imports "filename" (a csv file) into the existing population. Does an index based on the first column of the data
-        Empty values will be set to '0' for convinience.
+        Empty values will be set to '0'.
+
+        Arg:
+        'filename' string: string name of the file to be read
         """
-        data_object = csv(filename)  #Deal with exceptions
-        data_object.extract_data()
+        data_object = csv()  #Deal with exceptions
+        data_object.extract_data(filename)
 
         before_column = len(self.columns)
-        for i in data_object.columns: #For Column Index
-            self.columns.append(i)
+        for i in range(1,len(data_object.columns)): #For Column Index
+            self.columns.append(data_object.columns[i])
 
         """
         If the population is empty, simply add the csv file into the population.
@@ -24,10 +28,12 @@ class population(csv):
         if len(self.data) == 0:
             self.data = data_object.data
             self.list_of_countries = [data_object.data[i][0] for i in range(len(data_object.data))]
+            self.columns.insert(0,data_object.columns[0])
+            logging.debug('Population is empty. Added %s into the population object' %filename)
 
         else:
             """
-            If the population is not empty, properly indexes the country's data values and adds it into the existing population
+            If the population is not empty, indexes the country's data values and adds it into the existing population
             """
             for i in range(len(data_object.data)):
                 if data_object.data[i][0] in self.list_of_countries:
@@ -44,22 +50,26 @@ class population(csv):
                         self.data[-1].append(0)
                     for k in range(1,len(data_object.data[i])):
                         self.data[-1].append(data_object.data[i][k])
+                    logging.debug('New country %s found.' %data_object.data[i][0])
 
             """
-            Data cleaning
+            Data cleaning: Any missing data is set to the default value of 0
             """
             for i in self.data:
                 no_of_lines = len(self.columns)
                 csv.data_cleaning(self,i,no_of_lines)
 
+        logging.debug('Added %s into the population object' %filename)
 
 """ For debugging """
 if __name__ == '__main__':
     all_csv = population()
+    all_csv.import_csv("2015_genderinequality_csv_cleaned.csv")
     all_csv.import_csv("2015_HDI_csv_cleaned.csv")
     print(all_csv)
-    all_csv.import_csv("2015_genderinequality_csv_cleaned.csv")
-    print(all_csv)
+    for i in range(len(all_csv.columns)):
+        print(i+1,all_csv.columns[i])
+    print(len(all_csv.list_of_countries))
 
 
 
--- FinalProject/read_csv.py
+++ FinalProject/read_csv.py
@@ -1,29 +1,40 @@
+import logging
+
 class csv():
-    def __init__(self,filename):
+    def __init__(self):
         """
-        Initalize.
+        Arg:
+        'filename' string: string name of the file to be read
         """
-        self.fh = open(filename, 'r')
         self.columns = []
         self.data = []
+        logging.basicConfig(filename="log.txt",
+                            filemode='a',
+                            format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                            datefmt='%H:%M:%S',
+                            level=logging.DEBUG)
 
-    def extract_data(self):
+    def extract_data(self,filename):
+        fh = open(filename, 'r')
+        logging.debug('Attempting to extract %s' %filename)
+
         """
         Extracts the columns title into "self.columns" & the dataset into "self.data"
         """
-        self.columns = self.fh.readline().strip().split(",")
-        for line in self.fh:
+        self.columns = fh.readline().strip().split(",")
+        for line in fh:
             items = line.strip().split(',')
             self.data.append(items)
 
         """
-        Data cleaning: Sets any missing data to 0
+        1st Data cleaning: Any missing data is set to the default value of 0
         """
-        no_of_lines = len(self.columns)
+        no_of_variables = len(self.columns)
         for i in self.data:
-            self.data_cleaning(i,no_of_lines)
+            self.data_cleaning(i,no_of_variables)
 
         """
+        2nd Data cleaning:
         Changes all numerical values to a float type. 
         If the value is empty or some form of filler (eg. '...' & ' - '). Sets value to 0
         """
@@ -36,16 +47,36 @@ class csv():
                 except:
                     self.data[i][j] = 0
 
-    def data_cleaning(self,i,no_of_lines):
         """
-        Data cleaning: Sets any missing data to 0. 'i' refers to an entire row of country
+        Debugging Line:
+        """
+        logging.debug("Extracted %s" %filename)
+
+        """
+        Close File
+        """
+        fh.close()
+    def data_cleaning(self,i,no_of_variables):
+        """
+        Data cleaning: For data sets that are missing values/incomplete, appends a default value of '0'.
+
+        Arg:
+        'i' list: Row of data that is to be cleaned
+        'no_of_lines' integer: No. of variables in the data set.
+
+        Returns:
+        Cleaned row of data, where there are no missing values.
         """
 
-        if len(i) == no_of_lines:
+        if len(i) == no_of_variables:
             pass
-        elif len(i) < no_of_lines:
-                for j in range(no_of_lines - len(i)):
+        elif len(i) < no_of_variables:
+                for j in range(no_of_variables - len(i)):
                     i.append(0)
+                    """
+                    Debugging Line: To show that data cleaning has been performed, and on which line
+                    """
+                logging.debug("%s row has been cleaned." %i)
 
 
     def print_columns(self):
@@ -57,7 +88,7 @@ class csv():
 
     def print_data(self):
         """
-        rints dataset. Not as useful, since we have __str__
+        Prints dataset. Not as useful, since we have __str__
         """
         for i in range(len(self.data)):
             print("{0:10s}".format(self.data[i][0][0:10]), end=" ")
@@ -65,11 +96,6 @@ class csv():
                 print("{0:2f}".format(self.data[i][j]), end=" ")
             print("\n")
 
-    def close_file(self):
-        """
-        Closes file
-        """
-        self.fh.close()
 
     def __str__(self):
         """
@@ -84,7 +110,6 @@ class csv():
             for j in range(1,len(self.data[0])):
                 text += "{0:10.2f}".format(self.data[i][j]) + ' '
             text += '\n'
-
         return text
 
 
@@ -92,16 +117,6 @@ class csv():
 
 """ For debugging """
 if __name__ == '__main__':
-    k = csv("2015_genderinequality_csv_cleaned.csv")
-    k.extract_data()
+    k = csv()
+    k.extract_data("2015_genderinequality_csv_cleaned.csv")
     print(k)
-    k.close_file()
-
-"""Ideas:
-1. We will ask a series of questions, for example:
-"On a scale of 1 - 10, how impt do you think labour force participation rate matters for geneder equality?"
-We will use this numbers to do some computation, using existing data.
-
-Then we could come out with our own ranking of gender equality
-
-"""
